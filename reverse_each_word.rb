139b9487c1a45484f71c8ac85e910ea892fe2738
Michael Z <mziacm@gmail.com>
2019-10-11 02:49:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,25 @@
 def reverse_each_word(string)
   split_string = string.split(" ")
   reversed_array = []
-  reversed_array << split_string.pop
+  split_string.each {|word| reversed_arrayword.reverse
+  
+  reversed_array << split_string.pop}
   
 end
+
+
+def reverse_each_word(string)
+split_string = string.split(" ")
+  return_array = []
+  split_string.each {|item|
+    return_array.push(item.reverse)}
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+split_string = string.split(" ")
+  return_array = []
+  split_string.collect {|item|
+    return_array.push(item.reverse)}
+  return_array.join(" ")
+end 
